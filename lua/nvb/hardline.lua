36e321a6f314ad1d25ba80cd1fa5f4c86e105c00
avi0010 <aveekal@gmail.com>
2022-05-20 13:02:43 +0530

    till gitsigns

---
+++ lua/nvb/hardline.lua
@@ -0,0 +1,33 @@
+local gps = require("nvim-gps")
+
+gps.setup()
+
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = {},
+    section_separators = {},
+    disabled_filetypes = {},
+    always_divide_middle = true,
+    globalstatus = false,
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename', { gps.get_location, cond = gps.is_available }},
+    lualine_x = {'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {}
+}
---
+++ lua/nvb/startify.lua
@@ -0,0 +1,73 @@
+local status_ok, alpha = pcall(require, "alpha")
+if not status_ok then
+  return
+end
+
+-- local theme_ok, alpha_theme = pcall(require, "alpha.themes.startify")
+-- if not theme_ok then
+--  return
+-- end
+
+local dashboard = require "alpha.themes.dashboard"
+local function header()
+  return {
+    [[        /\          /\          /\       ]],
+    [[     /\//\\/\    /\//\\/\    /\//\\/\    ]],
+    [[  /\//\\\///\\/\//\\\///\\/\//\\\///\\/\ ]],
+    [[ //\\\//\/\\///\\\//\/\\///\\\//\/\\///\\]],
+    [[ \\//\/                            \/\\//]],
+    [[  \/                                  \/ ]],
+    [[  /\                                  /\ ]],
+    [[ //\\  Neovim IDE with LSP and DAP   //\\]],
+    [[ \\//                                \\//]],
+    [[  \/                                  \/ ]],
+    [[  /\                                  /\ ]],
+    [[ //\\/\                            /\//\\]],
+    [[ \\///\\/\//\\\///\\/\//\\\///\\/\//\\\//]],
+    [[  \/\\///\\\//\/\\///\\\//\/\\///\\\//\/ ]],
+    [[      \/\\//\/    \/\\//\/    \/\\//\/   ]],
+    [[         \/          \/          \/      ]],
+  }
+end
+dashboard.section.header.val = header()
+dashboard.section.buttons.val = {
+  dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+  dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+  dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
+  dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+  dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+  dashboard.button("c", "  Configuration", ":e $MYVIMRC<CR>"),
+  dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+}
+local function footer()
+  -- Number of plugins
+  local total_plugins = 121
+  local datetime = os.date "%d-%m-%Y %H:%M:%S"
+  local plugins_text = "   "
+    .. total_plugins
+    .. " plugins"
+    .. "   v"
+    .. vim.version().major
+    .. "."
+    .. vim.version().minor
+    .. "."
+    .. vim.version().patch
+    .. "   "
+    .. datetime
+
+  -- Quote
+  local fortune = require "alpha.fortune"
+  local quote = table.concat(fortune(), "\n")
+
+  return plugins_text .. "\n" .. quote
+end
+-- alpha.setup(alpha_theme.config)
+dashboard.section.footer.val = footer()
+
+dashboard.section.footer.opts.hl = "Constant"
+dashboard.section.header.opts.hl = "Include"
+dashboard.section.buttons.opts.hl = "Function"
+dashboard.section.buttons.opts.hl_shortcut = "Type"
+dashboard.opts.opts.noautocmd = true
+
+alpha.setup(dashboard.opts)
