f00870983a30be5a968d6c178eb788e17ce49ffd
avi0010 <aveekal@gmail.com>
2022-06-02 00:23:31 +0530

    home

--- lua/nvb/lsp/handlers.lua
+++ lua/nvb/lsp/handlers.lua
@@ -90,10 +90,6 @@ M.on_attach = function(client, bufnr)
   if client.name == "tsserver" then
     client.resolved_capabilities.document_formatting = false
   end
-  if client.name == "pyright" then
-    client.resolved_capabilities.document_formatting = false
-    client.resolved_capabilities.document_range_formatting = false
-  end
   lsp_keymaps(bufnr)
   lsp_highlight_document(client)
 end
--- lua/nvb/plugins.lua
+++ lua/nvb/plugins.lua
@@ -59,6 +59,7 @@ return packer.startup(function(use)
   use "folke/which-key.nvim"
   use "vimwiki/vimwiki"
   use "danymat/neogen"
+  use "rcarriga/nvim-notify"
 
   -- Colorschemes
   use "lunarvim/darkplus.nvim"
