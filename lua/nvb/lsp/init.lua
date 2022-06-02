3e504871cb208e112a8da657ca47d3fcb876ced5
aveekal <aveekal@gmail.com>
2022-06-02 18:18:32 +0530

    merge

--- lua/nvb/lsp/init.lua
+++ lua/nvb/lsp/init.lua
@@ -3,6 +3,10 @@ if not status_ok then
 	return
 end
 
-require("nvb.lsp.lsp-installer")
+-- require("nvb.lsp.lsp-installer")
+
 require("nvb.lsp.handlers").setup()
+require "nvb.lsp.config"
 require "nvb.lsp.null-ls"
+
+vim.notify("LSP loaded")
